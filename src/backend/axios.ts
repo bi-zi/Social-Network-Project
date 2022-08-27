64f3714e2d74f70d533488938e96dec4c5f5c780
bi zi <thebizi15@gmail.com>
2022-08-27 21:09:52 +0300

    Minor update

--- src/backend/axios.ts
+++ src/backend/axios.ts
@@ -1,11 +1,12 @@
 import axios from 'axios';
 
-
+const port: string = 'https://backend-for-wave.herokuapp.com/' || 'http://localhost:4444';
 
 const instance = axios.create({
-  baseURL: 'https://backend-for-wave.herokuapp.com/' || 'http://localhost:4444',
-
+  baseURL: port,
 });
+
+console.log(port)
 
 instance.interceptors.request.use((config: any) => {
   config.headers.Authorization = window.localStorage.getItem('token');
