a241aa5a4596c71280a285e410923b170f49b981
bi zi <thebizi15@gmail.com>
2022-08-27 18:24:18 +0300

    deploy to heroku

--- src/backend/axios.ts
+++ src/backend/axios.ts
@@ -1,7 +1,10 @@
 import axios from 'axios';
 
+
+
 const instance = axios.create({
-  baseURL: 'http://localhost:4444',
+  baseURL: 'https://backend-for-wave.herokuapp.com/' || 'http://localhost:4444',
+
 });
 
 instance.interceptors.request.use((config: any) => {
